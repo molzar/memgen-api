d035d81662b42d4f7cd2ee69cfcacdd7a0d07bd6
Molzar <bogdanstefan.info@gmail.com>
2019-01-14 11:13:22 +0200

    Uncomment text

--- api/utils/helper.js
+++ api/utils/helper.js
@@ -29,55 +29,6 @@ Helper.genericCallDB = function(query) {
     }) ;
 };
 
-
-// const p1 =  new Promise(function(resolve, reject) {
-//     pool.connect(function (err, client, done) {
-//         // Handle connection errors
-//         if (err) {
-//             done();
-//             console.log(err);
-//             //return res.status(500).json({success: false, data: err});
-//             return {success: false, data: err};
-//         }
-//         client.query(query)
-//             .then(result => {
-//                 done();
-//                 resolve({success: true, data: result.rows});
-//             })
-//             .catch(e => {
-//                 done();
-//                 reject({success: false, data: e.message});
-//             });
-//     });
-//     pool.end();
-// }) ;
-
-// const p2 =  new Promise(function(resolve, reject) {
-//     pool.connect(function (err, client, done) {
-//         // Handle connection errors
-//         if (err) {
-//             done();
-//             console.log(err);
-//             //return res.status(500).json({success: false, data: err});
-//             return {success: false, data: err};
-//         }
-//         client.query(query)
-//             .then(result => {
-//                 done();
-//                 resolve({success: true, data: result.rows});
-//             })
-//             .catch(e => {
-//                 done();
-//                 reject({success: false, data: e.message});
-//             });
-//     });
-//     pool.end();
-// }) ;
-
-// Promise.all([p1, p2], function(response1, response2) {
-
-// });
-
 Helper.genericHandlerCallDB = function(query, res){
 
     Helper.genericCallDB(query)
